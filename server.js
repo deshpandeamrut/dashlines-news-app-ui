d3d8ddba8784e920a3f04095314ffc90c79a4153
Amrut <XKS9@eurofins.com>
2019-04-30 14:55:27 +0530

    changes for heroku deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/dashlines'));
+app.use(express.static(__dirname + '/dist/angular6-start'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dashlines/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular6-start/index.html'));
 });
 
 // default Heroku PORT
