7c2c191212ff3d51587e69010359686cbdf50c85
Amrut <XKS9@eurofins.com>
2019-05-06 13:45:16 +0530

    api endpoint change

--- src/app/globalConstants.ts
+++ src/app/globalConstants.ts
@@ -1,4 +1,4 @@
 'use strict';
 
-//export const baseurl: string="https://obscure-shore-63169.herokuapp.com";    
-export const baseurl: string="http://localhost:8080";    
+export const baseurl: string="https://obscure-shore-63169.herokuapp.com";    
+//export const baseurl: string="http://localhost:8080";    
