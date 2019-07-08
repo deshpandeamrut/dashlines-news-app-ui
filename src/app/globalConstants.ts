8dc529085f35759a707bf9c828ce5de84dc2463f
Amrut <XKS9@eurofins.com>
2019-07-08 19:08:49 +0530

    CHanging Call to Ec2

--- src/app/globalConstants.ts
+++ src/app/globalConstants.ts
@@ -1,4 +1,5 @@
 'use strict';
 
-export const baseurl: string="https://obscure-shore-63169.herokuapp.com";    
+//export const baseurl: string="https://obscure-shore-63169.herokuapp.com";    
 //export const baseurl: string="http://localhost:8080";    
+export const baseurl: string="http://myfirstapp-env.wqu73pitvy.us-west-2.elasticbeanstalk.com";    
