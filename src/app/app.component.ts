7581dd9bf2737fa144b1a11331ec1db408f44a0e
Amrut <XKS9@eurofins.com>
2019-07-30 12:27:33 +0530

    Fix Local calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,36 +19,7 @@ export class AppComponent {
   
   categories = [];
  constructor(private http: Http) { }
-   ngOnInit() {
-	  this.http.get("http://localhost:8080/user/getCategories").subscribe((res)=>{
-	  console.log(this.categories = res.json()	)});
-      
-	  this.http.get("http://localhost:8080/news/getLatest").subscribe((res)=>{
-	  console.log(this.latestArticles = res.json()	)
-	  for(var i=0;i<this.latestArticles.length;i++){
-		  console.log(i);
-			this.featuredArticles.push(this.latestArticles[i]);
-			if(i==2){
-				break;
-			}
-	  }
-	  });
-	   
-	  this.http.get("http://localhost:8080/news/myFeed").subscribe((res)=>{
-	  console.log(this.myFeedArticles = res.json()	)
-	  });
-	 
-   }
-   
-     changeFeed(category){
-		 console.log("Changing feed to "+category);
-	  this.http.get("http://localhost:8080/news/category/"+category).subscribe((res)=>{
-	  console.log(this.latestArticles = res.json()	)});
-}
-   searchFeed(){
-	console.log("Searching for "+this.searchText);
-	  this.http.get("http://localhost:8080/news/category/"+this.searchText).subscribe((res)=>{
-	  console.log(this.latestArticles = res.json()	)});
-   }
+ 
+  
 
 }
