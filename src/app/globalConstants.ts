7b43b3ccdf975ab9aa902fa34f8ff7bc7102ae74
Amrut Deshpande <amrutdeshpande7@gmail.com>
2019-11-15 16:34:10 +0530

    Changing to Gateway

--- src/app/globalConstants.ts
+++ src/app/globalConstants.ts
@@ -3,4 +3,4 @@
 //export const baseurl: string="https://obscure-shore-63169.herokuapp.com";    
 //export const baseurl: string="http://localhost:8080";    
 //export const baseurl: string="http://myfirstapp-env.wqu73pitvy.us-west-2.elasticbeanstalk.com";    
-export const baseurl: string="https://dashlines-news-service.herokuapp.com/";    
+export const baseurl: string="https://dashlines-web-gateway.herokuapp.com/dashlines-service/";    
