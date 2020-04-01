082f49092514106438d5e3ca4289e2abd10c383c
Amrut Deshpande <amrutdeshpande7@gmail.com>
2020-04-01 10:44:58 +0530

    Update globalConstants.ts

--- src/app/globalConstants.ts
+++ src/app/globalConstants.ts
@@ -4,6 +4,6 @@
 //export const baseurl: string="http://localhost:8080";    
 //export const baseurl: string="http://myfirstapp-env.wqu73pitvy.us-west-2.elasticbeanstalk.com";    
 //export const baseurl: string="https://dashlines-web-gateway.herokuapp.com/dashlines-service/";    
-//export const baseurl: string="https://dashlines-news-service.herokuapp.com/"; 
-export const baseurl: string="http://13.233.229.22:8095/";
+export const baseurl: string="https://dashlines-news-service.herokuapp.com/"; 
+//export const baseurl: string="http://13.233.229.22:8095/"; //aws
 
